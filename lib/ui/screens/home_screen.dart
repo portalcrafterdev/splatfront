2a98819e6ed90afc0b85e4a65566420d28c8fc07
portalcrafterdev <softrpro007@gmail.com>
2026-09-03 19:47:45 +0530

    Adjust padding in _BandChip widget for improved layout consistency

--- lib/ui/screens/home_screen.dart
+++ lib/ui/screens/home_screen.dart
@@ -979,7 +979,7 @@ class _BandChip extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) => Container(
-    padding: const EdgeInsets.symmetric(horizontal: 9, vertical: 4),
+    padding: const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
     decoration: BoxDecoration(
       color: Palette.uiSurface,
       borderRadius: BorderRadius.circular(20),
